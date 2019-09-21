772e57fc4d43988245ea22cf99c6a0c63654c689
Austin <1424679344@qq.com>
2019-09-21 20:43:22 +0800

    first day

--- src/index.js
+++ src/index.js
@@ -4,3 +4,17 @@ return total
 }
 var TestNumber= addNumber(5,6)
 console.log(TestNumber)
+// if (true){
+//     console.log('true')
+// } else {
+//     console.log('false')
+// }
+if (addNumber >20){
+    console.log('true')
+} else{
+    console.log('false')
+}
+for(i=1;i<100;i++){
+var current=i
+console.log(current)
+}
